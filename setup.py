675a2929764f766a24c518b893f4f959224011d5
Ludrao <ludrao@ludrao.net>
2016-04-03 22:15:47 +0200

    Fix a pypi packaging issue

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tellme',
-    version='0.3',
+    version='0.4',
     packages=['tellme'],
     include_package_data=True,
     license='BSD License',
