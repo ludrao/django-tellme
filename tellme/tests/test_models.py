192c75c6bcbef01a721fc53e6525f9bdf7f76ff5
Anthony Monthe <anthony.monthe@gmail.com>
2018-05-09 05:09:27 +0100

    Added test

--- tellme/tests/test_models.py
+++ tellme/tests/test_models.py
@@ -4,8 +4,13 @@ from tellme.tests.factories import FeedbackFactory
 
 
 class FeedbackScreenshotDeleteTest(TestCase):
-    def test_signal(self):
+    def test_delete_screenshot(self):
         feedback = FeedbackFactory()
         self.assertTrue(feedback.screenshot)
+        feedback.delete()
+        self.assertFalse(feedback.screenshot)
+
+    def test_no_screenshot(self):
+        feedback = FeedbackFactory(screenshot=None)
         feedback.delete()
         self.assertFalse(feedback.screenshot)
