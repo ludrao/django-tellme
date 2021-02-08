2ecd706bd97cd2c188c0a1c94bd934a3e8a625da
Gordon Pendleton <wgordonw1@gmail.com>
2021-02-08 12:00:08 -0500

    start work building feedback with CI

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = [
+    {
+      entry: ['./tellme/static/tellme/feedback/script.js'],
+      module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ],
+      },
+      resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
+      output: {
+        filename: 'feedback.js',
+        path: path.resolve(__dirname, 'dist'),
+      },
+      optimization: {
+        minimize: false
+      }
+    },
+    {
+      entry: ['./tellme/static/tellme/feedback/style.js'],
+      module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader'
+                ],
+            },
+        ],
+      },
+      plugins: [
+        new MiniCssExtractPlugin({
+            filename: "feedback.css"
+        })
+      ],
+      resolve: {
+        extensions: ['.css', '.scss'],
+      },
+      output: {
+        path: path.resolve(__dirname, 'dist'),
+      },
+      optimization: {
+        minimize: false
+      }
+    }
+];
