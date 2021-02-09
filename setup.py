8b87c326bd90b5c070468bd9203b2c732a64ec1a
ludrao <ludrao@ludrao.net>
2021-02-09 23:01:45 +0100

    Prepare versioning for next release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tellme',
-    version='0.6.7',
+    version='0.7.0',
     packages=['tellme'],
     include_package_data=True,
     license='BSD License',
